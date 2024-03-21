7b2b9db8948430c07b13683487c8c833e885d6e9
Steinbächer <DESTEIMI@de.eu.sew>
2024-03-21 17:18:49 +0100

    images

--- spielfenster.cpp
+++ spielfenster.cpp
@@ -1,11 +1,17 @@
 #include "spielfenster.h"
 #include "ui_spielfenster.h"
+#include "QPixmap"
 
 Spielfenster::Spielfenster(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Spielfenster)
 {
     ui->setupUi(this);
+
+    QString imagePath = QCoreApplication::applicationFilePath() + "/images/background.png";
+    QPixmap pix(imagePath);
+    ui->SpinningWheel1->setPixmap(pix);
+    ui->SpinningWheel2->setText(imagePath);
 }
 
 Spielfenster::~Spielfenster()
